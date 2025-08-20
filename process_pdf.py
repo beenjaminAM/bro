cfb7809f8ee1e99cc956fe1f3cb99586da224e2a
beenjaminAM <benjamin.ancaya@unmsm.edu.pe>
2025-08-19 19:19:12 -0500

    process pdf to embedding

--- process_pdf.py
+++ process_pdf.py
@@ -8,8 +8,7 @@ from embedding_db import insert_in_collection, create_vector_collection, create_
 
 def process_pdf_to_embedding(pdf_path, collection_name ,logs_errors_df, logs_pdfs_list):
 
-    # Call the function
-    result_text, updated_logs = extract_cleaned_text_until_index_page(
+    result_text, _ = extract_cleaned_text_until_index_page(
         pdf_path=pdf_path,
         filename= os.path.basename(pdf_path).replace(".pdf", ""),
         logs_df=logs_errors_df,
@@ -20,10 +19,26 @@ def process_pdf_to_embedding(pdf_path, collection_name ,logs_errors_df, logs_pdf
 
     if result_text is None:
         raise Exception('pdf limiters error')
+
+    id = 0
+
+    pdf_df = logs_pdfs_list
+    if len(pdf_df) > 0:
+        id = pdf_df['id'].max()
     
-    vectors = vectorize_text(result_text)
+    text_chunks = chunk_text(result_text)
+    vectors = get_embedding(text_chunks)
+    ids = [f"{id}-{i}" for i in range(len(vectors))]
+    metadatas = [{"article": id} for _ in range(len(vectors))]
 
+    insert_in_collection(collection_name, text_chunks, metadatas, ids, vectors)
 
+    filename = os.path.basename(pdf_path).replace(".pdf", "")
+    if filename not in logs_pdfs_list['name'].to_list():
+        id = id
+        description = f"{filename}"
+        new_log_row = pl.DataFrame({'id': [id], 'name': [f"{description}"]}, schema={'id': pl.Int64, 'name': pl.Utf8})
+        pl.concat([logs_pdfs_list, new_log_row], how="vertical")
 
 if __name__ == '__main__':
 
