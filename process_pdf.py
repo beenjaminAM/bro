467e13ef9bae66cdd255df9349a5dfc9bf1bf45d
beenjaminAM <benjamin.ancaya@unmsm.edu.pe>
2025-08-19 19:18:42 -0500

    process pdf imports

--- process_pdf.py
+++ process_pdf.py
@@ -1,9 +1,10 @@
-from loggings import create_logging_errors
+from loggings import create_logging_errors, create_logging_pdf_list
 import os
-from vectorize_text import vectorize_text
+import polars as pl
+from embedding_model import get_embedding
+from chunks import chunk_text
 from clean_pdf import extract_cleaned_text_until_index_page
-from vector_db import insert_in_collection
-
+from embedding_db import insert_in_collection, create_vector_collection, create_vector_db
 
 def process_pdf_to_embedding(pdf_path, collection_name ,logs_errors_df, logs_pdfs_list):
 
